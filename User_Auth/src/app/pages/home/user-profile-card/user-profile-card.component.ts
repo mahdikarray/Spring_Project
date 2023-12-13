b40fa9e3bcd0cd05404c89456e6b9c6cd0fea216
MohamedMokhtarHadded <mohamedmokhtar.hadded@esprit.tn>
2023-12-13 10:41:06 +0100

    User auth

---
+++ User_Auth/src/app/pages/home/user-profile-card/user-profile-card.component.ts
@@ -0,0 +1,11 @@
+import { Component ,Input} from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-card',
+  templateUrl: './user-profile-card.component.html',
+  styleUrls: ['./user-profile-card.component.scss']
+})
+export class UserProfileCardComponent {
+  @Input() etudiant:any;
+
+}
